523de6106c3c33859c626b77eb837d47f66ce3eb
Luissa Vahedi <l6lv6@queensu.ca>
2026-04-19 17:37:26 -0400

    Luissa assignment 1 v5

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -59,7 +59,7 @@ cp raw/event*.log processed/event_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 cd raw
-rm -i ipaddr*.txt
+rm ipaddr*.txt
 rm user_ipaddr*.log
 
 cd ../processed
